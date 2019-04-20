fbdc23edc284d1a99478677a72c0bbd6c9c95c8d
Hsin <wuhsin@zju.edu.cn>
2019-04-20 20:32:03 +0800

    .

--- source/conf.py
+++ source/conf.py
@@ -64,5 +64,6 @@ html_static_path = ['_static']
 
 ## reload theme ##
 # import sphinx_rtd_theme
-# html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+master_doc = 'index'
