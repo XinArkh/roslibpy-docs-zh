92fc244972531db0c594b88fd22c93dcbe7a0030
Hsin <wuhsin@zju.edu.cn>
2019-04-20 13:38:56 +0800

    test original conf

--- source/conf.py
+++ source/conf.py
@@ -63,6 +63,6 @@ html_static_path = ['_static']
 
 
 ## reload theme ##
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# import sphinx_rtd_theme
+# html_theme = "sphinx_rtd_theme"
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
