8346fcd57721ffc07f3c1515f50f7040c88d9c5e
Hsin <wuhsin@zju.edu.cn>
2019-06-09 14:00:14 +0800

    .

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 # sys.path.insert(0, os.path.abspath('../src'))
 # in readthedocs:
-sys.path.insert(0, './src')
+sys.path.insert(0, os.path.abspath('./src'))
 
 
 # -- Project information -----------------------------------------------------
